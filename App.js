eae52aabb43c8b135d5db24836137d78c30118d9
ouskxk <01.hyeona@gmail.com>
2025-05-10 02:23:59 +0900

    5월 10일 수정

--- App.js
+++ App.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Main from './main';
-import Login from './login';
-import Home from './home';
-import Result from './result';
-import Comm from './comm';
-import Daily from './daily';
-import Test from './test'; 
-import Write from './write';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Main from "./main";
+import Login from "./login";
+import Home from "./home";
+import Result from "./result";
+import Comm from "./comm";
+import Daily from "./daily";
+import Test from "./test";
+import Write from "./write";
+import Detail from "./detail";
+import { PostProvider } from "./postContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/comm" element={<Comm />} />
-        <Route path="/daily" element={<Daily />} />
-        <Route path="/test" element={<Test />} />
-        <Route path="/write" element={<Write />} />
-      </Routes>
-    </Router>
+    <PostProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/result" element={<Result />} />
+          <Route path="/comm" element={<Comm />} />
+          <Route path="/daily" element={<Daily />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="/write" element={<Write />} />
+          <Route path="/write/:id" element={<Write />} />
+          <Route path="/detail/:id" element={<Detail />} />
+        </Routes>
+      </Router>
+    </PostProvider>
   );
 }
 
