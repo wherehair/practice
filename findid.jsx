89f6dc3a6fb8546512a6288b85328d1a664e4433
suerte2230 <rhwldns0322@gmail.com>
2025-05-31 23:34:36 +0900

    아이디찾기임다

---
+++ findid.jsx
@@ -0,0 +1,8 @@
+export default function FindId() {
+  return (
+    <div style={{ padding: "50px", textAlign: "center" }}>
+      <h2>아이디 찾기</h2>
+      <p>아이디 찾기 기능은 준비 중입니다.</p>
+    </div>
+  );
+}
