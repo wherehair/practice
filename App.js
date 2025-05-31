33a2e072dfce3f4a8d313664455dd3db7fdc3300
suerte2230 <rhwldns0322@gmail.com>
2025-05-31 23:35:21 +0900

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,48 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Main from "./main";
-import Login from "./login";
-import Home from "./home";
-import Result from "./result";
-import Comm from "./comm";
-import Daily from "./daily";
-import Test from "./test";
-import Write from "./write";
-import Detail from "./detail";
-import { PostProvider } from "./postContext";
+import React from 'react';
+import { ProfileProvider } from './Component/profileContext';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import Main       from './Component/main';
+import Login      from './Component/login';
+import Home       from './Component/home';
+import Result     from './Component/result';
+import Comm       from './Component/comm';
+import Daily      from './Component/daily';
+import Test       from './Component/test';
+import Write      from './Component/write';
+import Detail     from './Component/detail';
+import { PostProvider } from './Component/postContext';
+import Signup from './Component/signup';
+import FindId from './Component/findid';
+import FindPw from './Component/findpw';
 
 function App() {
   return (
-    <PostProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/result" element={<Result />} />
-          <Route path="/comm" element={<Comm />} />
-          <Route path="/daily" element={<Daily />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/write" element={<Write />} />
-          <Route path="/write/:id" element={<Write />} />
-          <Route path="/detail/:id" element={<Detail />} />
-        </Routes>
-      </Router>
-    </PostProvider>
+    <ProfileProvider>
+      <PostProvider>
+        <Router>
+          <Routes>
+            <Route path="/"      element={<Navigate to="/main" replace />} />
+            <Route path="/main"  element={<Main />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/home"  element={<Home />} />
+            <Route path="/result"element={<Result />} />
+            <Route path="/comm"  element={<Comm />} />
+            <Route path="/daily" element={<Daily />} />
+            <Route path="/test"  element={<Test />} />
+            <Route path="/write"     element={<Write />} />
+            <Route path="/write/:id" element={<Write />} />
+            <Route path="/detail" element={<Detail />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/find-id" element={<FindId />} />
+            <Route path="/find-pw" element={<FindPw />} />
+            
+          </Routes>
+        </Router>
+      </PostProvider>
+    </ProfileProvider>
   );
 }
+
 
 export default App;
