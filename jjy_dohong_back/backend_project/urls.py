382df6e1a5f93dfa9d4543f324afb74702f79ade
jjy0701 <86817506+jjy0701@users.noreply.github.com>
2025-06-03 23:10:22 +0900

    api 코드 및 urls 추가

--- jjy_dohong_back/backend_project/urls.py
+++ jjy_dohong_back/backend_project/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # include 추가
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/accounts/', include('accounts.urls')),  # accounts 앱 URL 포함
+    path('api/ai/', include('ai.urls')),              # ai 앱 URL 포함
 ]
